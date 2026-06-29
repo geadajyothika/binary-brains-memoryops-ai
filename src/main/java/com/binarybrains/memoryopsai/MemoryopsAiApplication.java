786b7c788a7ce3800f2ecf121f106fa0dd5010e5
geadajyothika <geadajyothika12@gmail.com>
2026-06-29 16:17:51 +0530

    Initial commit

---
+++ src/main/java/com/binarybrains/memoryopsai/MemoryopsAiApplication.java
@@ -0,0 +1,13 @@
+package com.binarybrains.memoryopsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryopsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryopsAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binarybrains/memoryopsai/MemoryopsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarybrains.memoryopsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryopsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
